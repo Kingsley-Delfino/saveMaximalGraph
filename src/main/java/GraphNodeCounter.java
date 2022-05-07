7e878e6e0d1b6b6ac84aba571c1b87dee209b307
Kingsley <2901704946@qq.com>
2022-05-07 16:28:43 +0800

    统计图中节点出现次数，并输出到文本文件

--- src/main/java/GraphNodeCounter.java
+++ src/main/java/GraphNodeCounter.java
@@ -1,3 +1,4 @@
+import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -5,23 +6,37 @@ import java.util.TreeMap;
 
 public class GraphNodeCounter {
 
-    private static final String GRAPH_PATH = "/Users/kingsley/FDSE/gSpan/graphdata/commons-io(1003)_file_k3_7_2_0415(Spliced_Link).txt";
+    private static final String GRAPH_PATH = "/Users/kingsley/FDSE/gSpan/graphdata/commons-io(1003)_file_k2_tw7_s3_0504";
+
+    private static final String MAXIMAL_GRAPH = "(Maximal_Graph)";
+
+    private static final String CHANGE_TIMES = "(Change_Times)";
+
+    private static final String TXT_SUFFIX = ".txt";
 
     public static void main(String[] args) {
-        // 从文件中读取所有的图
-        Set<Graph> graphSet = GraphUtils.readGraph(GRAPH_PATH);
-        Map<Integer, Integer> nodeTimesMap = new TreeMap<>();
-        for (Graph graph : graphSet) {
-            Set<Integer> nodeSet = graph.getNodes();
-            for (int node : nodeSet) {
-                int times = nodeTimesMap.getOrDefault(node, 0);
-                nodeTimesMap.put(node, times + 1);
+        try {
+            PrintStream terminal = System.out;
+            PrintStream ps = new PrintStream(GRAPH_PATH + CHANGE_TIMES + TXT_SUFFIX);
+            System.setOut(ps);
+            // 从文件中读取所有的图
+            Set<Graph> graphSet = GraphUtils.readGraph(GRAPH_PATH + MAXIMAL_GRAPH + TXT_SUFFIX);
+            Map<Integer, Integer> nodeTimesMap = new TreeMap<>();
+            for (Graph graph : graphSet) {
+                Set<Integer> nodeSet = graph.getNodes();
+                for (int node : nodeSet) {
+                    int times = nodeTimesMap.getOrDefault(node, 0);
+                    nodeTimesMap.put(node, times + 1);
+                }
             }
+            for (Map.Entry<Integer, Integer> entry : nodeTimesMap.entrySet()) {
+                System.out.println(entry.getKey() + "_" + entry.getValue());
+            }
+            System.setOut(terminal);
+            System.out.println("导出成功！");
         }
-        int index = 0;
-        for (Map.Entry<Integer, Integer> entry : nodeTimesMap.entrySet()) {
-            System.out.println(index + ": " + entry.getKey() + "(" + entry.getValue() + ")");
-            index ++;
+        catch (Exception e) {
+            System.out.println("导出失败！");
         }
     }
 }
